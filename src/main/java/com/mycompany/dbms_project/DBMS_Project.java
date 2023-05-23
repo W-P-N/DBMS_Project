36a5f5e123aa981a8acfcb0df216c47e6f51b02d
Parth <parthwani22@gmail.com>
2023-05-23 11:04:02 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/dbms_project/DBMS_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dbms_project;
+
+/**
+ *
+ * @author parth
+ */
+public class DBMS_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
